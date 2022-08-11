ebeb828de54039f381bd725511f42745bf12c7f2
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-08-12 00:31:04 +0200

    initial commit

--- app/js/script.js
+++ app/js/script.js
@@ -3,38 +3,29 @@
 // 12 points -> suspended
 // Math.floor (1.6)
 
-/* 
 function checkSpeed(speed) {
-    const speedLimit = 70;
-    const kmPerPoint = 5;
-    const maxPoints = 12;
-    if (speed < speedLimit + kmPerPoint) {
-        console.log('ok');
-        return
-    }
-    const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points >= maxPoints) {
-        console.log('License suspended')
-    } else {
-        console.log('Points:', points)
-    }
-
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  const maxPoints = 12;
+  if (speed < speedLimit + kmPerPoint) {
+    console.log("ok");
+    return;
+  }
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if (points >= maxPoints) {
+    console.log("License suspended");
+  } else {
+    console.log("Points:", points);
+  }
 }
 
-
-
-const array = [0, null, 2, 3, 34252, 2352, undefined, 123]
+const array = [0, null, 2, 3, 34252, 2352, undefined, 123];
 
 function countTruthy(array) {
-    let count = 0;
-    for (let value of array)
-        if (value)
-            count++
-    return count;
+  let count = 0;
+  for (let value of array) if (value) count++;
+  return count;
 }
 
 const message = countTruthy(array);
 console.log(message);
-*/
-
-
