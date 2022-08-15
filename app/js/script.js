cfb6c0b50e128528dbe0587121834ae1f7a9e70b
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-08-15 23:51:17 +0200

    added footer section

--- app/js/script.js
+++ app/js/script.js
@@ -2,3 +2,18 @@ window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".nav_container");
   navbar.classList.toggle("active", window.scrollY > 50);
 });
+
+const toggleMenu = document.querySelector(".mobile_nav-toggle");
+const navItem = document.querySelector(".nav_lists");
+
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("active");
+  navItem.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav_list-item").forEach((e) =>
+  e.addEventListener("click", () => {
+    toggleMenu.classList.remove("active");
+    navItem.classList.remove("active");
+  })
+);
